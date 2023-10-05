409946b9cbe518435edbd7793047deb7085ef49d
Jeremy <the.jeremy.cobb@gmail.com>
2023-10-05 16:35:31 -0500

    added cards that display movie title, movies poster, and a brief synopsis of said film

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,6 @@ const App = () => {
 
     // getting all films from api
     const [films, setFilms] = useState([]);
-
-    // useEffect(() => {
-    //     fetch('http://api-ghibli.herokuapp.com/films')
-    //         .then(response => response.json())
-    //         .then(allFilms => setFilms(allFilms))
-    // }, []);
-
     const getData = () => {
         fetch('http://api-ghibli.herokuapp.com/films')
                 .then(response => response.json())
@@ -23,7 +16,13 @@ const App = () => {
                 <h1>lmao hi</h1>
                 <button className="btn btn-primary" onClick={getData}>Load Films</button>
                 {films.map(film => (
-                    <p>{film.title}</p>
+                    <div className="card " style={{ width: "18rem" }}>
+                        <img src={film.image} className="card-img-top" />
+                        <div className="card-body">
+                            <h5 className="card-title">{film.title}</h5>
+                            <p className="card-text">{film.description}</p>
+                        </div>
+                    </div>
                 ))}
             </div>
         </>
