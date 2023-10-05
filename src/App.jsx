dabd29e8ba6752b05f195576dc03ce54670c7e16
Jeremy <the.jeremy.cobb@gmail.com>
2023-10-05 16:44:24 -0500

    added some light card styling with bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,13 @@ const App = () => {
     const [films, setFilms] = useState([]);
     const getData = () => {
         fetch('http://api-ghibli.herokuapp.com/films')
-                .then(response => response.json())
-                .then(allFilms => setFilms(allFilms))
+            .then(response => response.json())
+            .then(allFilms => setFilms(allFilms))
     };
 
     return (
         <>
-            <div>
+            {/* <div>
                 <h1>lmao hi</h1>
                 <button className="btn btn-primary" onClick={getData}>Load Films</button>
                 {films.map(film => (
@@ -24,7 +24,23 @@ const App = () => {
                         </div>
                     </div>
                 ))}
-            </div>
+            </div> */}
+            <main className="container">
+                <section className="row justify-content-center mt-5">
+                    <button className="btn btn-primary" onClick={getData}>Load Filmz</button>
+                    {films.map(film => (
+                        <div className="col-md-6" key={film.id}>
+                            <div className="card shadow my2" style={{width: "18rem"}}>
+                                <img src={film.image} className="card-img-top" />
+                                <div className="card-body">
+                                    <h5 className="card-title">{film.title}</h5>
+                                    <p className="card-text">{film.description}</p>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </section>
+            </main>
         </>
     );
 }
