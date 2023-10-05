4031382adb80c2fcceaabaebacec786276fa80db
Jeremy <the.jeremy.cobb@gmail.com>
2023-10-05 16:05:41 -0500

    added useEffects & useStates, fetched & displayed api data

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
+import { useEffect, useState } from "react";
+
 const App = () => {
-    return(
+
+    // getting all films from api
+    const [films, setFilms] = useState([]);
+
+    // useEffect(() => {
+    //     fetch('http://api-ghibli.herokuapp.com/films')
+    //         .then(response => response.json())
+    //         .then(allFilms => setFilms(allFilms))
+    // }, []);
+
+    const getData = () => {
+        fetch('http://api-ghibli.herokuapp.com/films')
+                .then(response => response.json())
+                .then(allFilms => setFilms(allFilms))
+    };
+
+    return (
         <>
             <div>
                 <h1>lmao hi</h1>
+                <button className="btn btn-primary" onClick={getData}>Load Films</button>
+                {films.map(film => (
+                    <p>{film.title}</p>
+                ))}
             </div>
         </>
     );
